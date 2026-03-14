11417aed831eebe7e4e41885f909fe4ee8ba365c
Subha Chandran <42923416+subhachandran25@users.noreply.github.com>
2026-03-14 08:54:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.tree import DecisionTreeRegressor
 
 st.set_page_config(layout="wide")
 
